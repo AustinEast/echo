78e70567f4cf4bf6d8cfe7f03fda791a9a3d6167
austin <eastin93@gmail.com>
2022-02-21 13:18:40 -0800

    module level methods

--- echo/util/Disposable.hx
+++ echo/util/Disposable.hx
@@ -1,36 +1,31 @@
 package echo.util;
 
-/**
- * This Class contains utility methods for Classes that implement `IDisposable`.
- */
-class Disposable {
-  /**
-   * Checks if an object is not null before calling dispose(), always returns null.
-   *
-   * @param	object	An IDisposable object that will be disposed if it's not null.
-   * @return	null
-   */
-  public static function dispose<T:IDisposable>(object:Null<IDisposable>):T {
-    if (object != null) {
-      object.dispose();
-    }
-    return null;
-  }
-  /**
-   * dispose every element of an array of IDisposables
-   *
-   * @param	array	An Array of IDisposable objects
-   * @return	null
-   */
-  public static function disposeArray<T:IDisposable>(array:Array<T>):Array<T> {
-    if (array != null) {
-      for (e in array) dispose(e);
-      array.splice(0, array.length);
-    }
-    return null;
-  }
-}
-
+@:using(echo.util.Disposable)
 interface IDisposable {
   function dispose():Void;
+}
+/**
+ * Checks if an object is not null before calling dispose(), always returns null.
+ *
+ * @param	object	An IDisposable object that will be disposed if it's not null.
+ * @return	null
+ */
+inline function dispose<T:IDisposable>(object:Null<IDisposable>):T {
+  if (object != null) {
+    object.dispose();
+  }
+  return null;
+}
+/**
+ * dispose every element of an array of IDisposables
+ *
+ * @param	array	An Array of IDisposable objects
+ * @return	null
+ */
+inline function disposeArray<T:IDisposable>(array:Array<T>):Array<T> {
+  if (array != null) {
+    for (e in array) dispose(e);
+    array.splice(0, array.length);
+  }
+  return null;
 }

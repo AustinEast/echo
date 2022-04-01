110f9108bef957b37518e52d602031c7eaa7c5f9
austin <eastin93@gmail.com>
2022-04-01 10:49:11 -0700

    hide friction

--- echo/Material.hx
+++ echo/Material.hx
@@ -12,13 +12,21 @@ class Material {
    * Value to determine how much of a Body's `velocity` should be retained during collisions (or how much should the `Body` "bounce" in other words).
    */
   public var elasticity:Float = 0;
-
+  /**
+   * 
+   */
   public var density:Float = 1;
-
-  // TODO
+  /**
+   * TODO
+   */
+  @:dox(hide)
+  @:noCompletion
   public var friction:Float = 0;
-
-  // TODO
+  /**
+   * TODO
+   */
+  @:dox(hide)
+  @:noCompletion
   public var static_friction:Float = 0;
   /**
    * Percentage value that represents how much a World's gravity affects the Body.

abf16b91e5cca4b23a76e67ddee171f797bbf810
Logan <monday.hopscotch@gmail.com>
2023-10-25 13:13:57 -0600

    fix ordering of shape collision data so listener data matches order of bodies

--- echo/shape/Circle.hx
+++ echo/shape/Circle.hx
@@ -87,13 +87,16 @@ class Circle extends Shape implements Poolable {
     return false;
   }
 
-  override inline function collides(s:Shape):Null<CollisionData> return s.collide_circle(this);
+  // collision calculated as s against this. So flip result
+  override inline function collides(s:Shape):Null<CollisionData> return s.collide_circle(this, true);
 
-  override inline function collide_rect(r:Rect):Null<CollisionData> return r.rect_and_circle(this, true);
+  // collision calculated as r against this. So invert flip value
+  override inline function collide_rect(r:Rect, flip:Bool = false):Null<CollisionData> return r.rect_and_circle(this, !flip);
 
-  override inline function collide_circle(c:Circle):Null<CollisionData> return c.circle_and_circle(this);
+  // collision calculated as c against this. So invert flip value
+  override inline function collide_circle(c:Circle, flip:Bool = false):Null<CollisionData> return c.circle_and_circle(this, !flip);
 
-  override inline function collide_polygon(p:Polygon):Null<CollisionData> return this.circle_and_polygon(p, true);
+  override inline function collide_polygon(p:Polygon, flip:Bool = false):Null<CollisionData> return this.circle_and_polygon(p, flip);
 
   // getters
   inline function get_radius():Float return local_radius * scale_x;
